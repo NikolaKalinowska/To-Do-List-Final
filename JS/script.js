afc4519b158e9e390988e557adc7a6da52f14f8c
Nikola Kalinowska <nikola.kalinowska1999@gmail.com>
2023-10-13 11:07:24 +0200

    Added some comments to my js code

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 {
+    // Tablica zadan
     const tasks = [
         {
             content: "test",
@@ -6,24 +7,41 @@
         },
         {
             content: "test",
-            done: true,
+            done: true, 
         },
     ];
 
+    // Funkcja do renderowania listy zadań
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
-            htmlString +=`
-            <li${task.done ? " style=\"text-decoration: line-through;\"" : ""}>
-                ${task.content}
-            </li>`;
+            htmlString += `
+                <li${task.done ? " style=\"text-decoration: line-through;\"" : ""}> 
+                    ${task.content}
+                </li>`;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    // Funkcja inicjalizacyjna
     const init = () => {
         render();
+
+        // Dodanie obsługi formularza
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault(); 
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            console.log(newTaskContent)
+
+            if (newTaskContent === "") {
+                return;
+            }
+        });
     };
 
+    // Inicjalizacja
     init();
 }
