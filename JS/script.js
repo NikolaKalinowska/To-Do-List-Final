c4ba7d606506e94851e482ca91cef921de4f1b3c
Nikola Kalinowska <nikola.kalinowska1999@gmail.com>
2023-10-13 13:12:58 +0200

    made chages in body in html and some chages in css

--- JS/script.js
+++ JS/script.js
@@ -1,93 +1,90 @@
 {
     // Tablica zadań
-    const tasks = [
-        {
-            content: "test",
-            done: false
-        },
-        {
-            content: "test",
-            done: true,
-        },
-    ];
+const tasks = [
+    {
+        content: "test",
+        done: false
+    },
+    {
+        content: "test",
+        done: true,
+    },
+];
 
-    // Funkcja dodająca nowe zadanie
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-            done: false
-        });
-        render();
+// Funkcja dodająca nowe zadanie
+const addNewTask = (newTaskContent) => {
+    tasks.push({
+        content: newTaskContent,
+        done: false
+    });
+    render();
+}
+
+// Funkcja usuwająca dane zadanie
+const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+}
+
+// Funkcja zmieniająca status zadania na zrobione
+const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+}
+
+// Funkcja do renderowania listy zadań
+const render = () => {
+    let htmlString = "";
+    for (const [index, task] of tasks.entries()) {
+        htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through;\"" : ""}> 
+                <button class="js-done" data-index="${index}">Zadanie zrobione</button>
+                <button class="js-remove" data-index="${index}">Usuń</button>
+                ${task.content}
+            </li>`;
     }
+    document.querySelector(".js-tasks").innerHTML = htmlString;
 
-    // Funkcja usuwająca dane zadanie
-    const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
-        render();
-    }
-
-    // Funkcja zmieniająca status zadania na zrobione
-    const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
-        render();
-    }
-
-    // Funkcja do renderowania listy zadań
-    const render = () => {
-        let htmlString = "";
-        for (const [index, task] of tasks.entries()) {
-            htmlString += `
-                <li${task.done ? " style=\"text-decoration: line-through;\"" : ""}> 
-                    <button class="js-done" data-index="${index}">Zadanie zrobione</button>
-                    <button class="js-remove" data-index="${index}">Usuń</button>
-                    ${task.content}
-                </li>`;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-        bindEvents(); // Dodane wywołanie funkcji bindEvents
-    };
-
-    const bindEvents = () => {
-        // Przycisk usuwający dany task
-        const removeButtons = document.querySelectorAll(".js-remove");
-        removeButtons.forEach((button) => {
-            button.addEventListener("click", (event) => {
-                const taskIndex = event.target.dataset.index;
-                removeTask(taskIndex);
-            });
+    // Przycisk usuwający dany task
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const taskIndex = event.target.dataset.index;
+            removeTask(taskIndex);
         });
+    });
 
-        // Przycisk oznaczający zadanie jako zrobione
-        const toggleDoneButtons = document.querySelectorAll(".js-done");
-        toggleDoneButtons.forEach((button) => {
-            button.addEventListener("click", (event) => {
-                const taskIndex = event.target.dataset.index;
-                toggleTaskDone(taskIndex);
-            });
+    // Przycisk oznaczający zadanie jako zrobione
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+    toggleDoneButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const taskIndex = event.target.dataset.index;
+            toggleTaskDone(taskIndex);
         });
-    };
+    });
+};
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
+const onFormSubmit = (event) => {
+    event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-        if (newTaskContent === "") {
-            return;
-        }
-        addNewTask(newTaskContent); // Dodanie nowego zadania
-    };
+    if (newTaskContent === "") {
+        return;
+    }
+    addNewTask(newTaskContent); // Dodanie nowego zadania
+};
 
-    // Funkcja inicjalizacyjna
-    const init = () => {
-        render();
+// Funkcja inicjalizacyjna
+const init = () => {
+    render();
 
-        // Dodanie obsługi formularza
-        const form = document.querySelector(".js-form");
+    // Dodanie obsługi formularza
+    const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", onFormSubmit); // Dodanie obsługi zdarzenia "submit"
-    };
+    form.addEventListener("submit", onFormSubmit); // Dodanie obsługi zdarzenia "submit"
+};
 
-    // Inicjalizacja
-    init();
+// Inicjalizacja
+init();
 }
